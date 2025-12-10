3b5d2af4163db29f8ac437bd096905c6acbf3683
David Rodriguez <drodriguez@stsci.edu>
2025-12-10 14:11:30 -0500

    adding build script, cleaning up unused records from json files

--- scripts/build_db.py
+++ scripts/build_db.py
@@ -5,10 +5,13 @@ Build the database from the JSON files.
 from astrodb_utils import build_db_from_json
 from astrodb_utils.loaders import DatabaseSettings
 
-def main():
+def main(save: bool = False):
     # Instantiate database from settings file
     db_settings = DatabaseSettings(settings_file="database.toml")
-    _ = build_db_from_json(settings_file=db_settings.settings_file)
+    db = build_db_from_json(settings_file=db_settings.settings_file)
+
+    if save:
+        db.save_database(directory="data")
 
 if __name__ == "__main__":
-    main()
+    main(save=False)
